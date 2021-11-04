dbb2a1eb9d8ccd2af9bcad227e1d53b857f23afd
Jeff Clement <jeff@zeos.ca>
2021-11-04 01:18:19 -0600

    making sure react can say hello

---
+++ client/src/Leaderboard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class Leaderboard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: []
+        };
+    }
+    componentDidMount() {
+        fetch('/leaderboard')
+            .then(response => response.json())
+            .then(data => this.setState({'data': data}))
+    }
+    render() {
+      return <table><tbody>
+          <tr><th>Name</th><th>Score</th></tr>
+              {this.state.data.map((row,index) => <tr key={row.user_id}>
+              <td>{row.username}</td>
+              <td>{row.score}</td>
+                  </tr>)}
+      </tbody></table>
+    }
+}
+
+export default Leaderboard
