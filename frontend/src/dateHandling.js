a38b19d49f1a9f40b6df2bef6b19c65f20f2730a
hfi580 <17411164+hfi580@users.noreply.github.com>
2021-11-21 15:15:10 -0700

    converting from utc to local for editors

--- frontend/src/dateHandling.js
+++ frontend/src/dateHandling.js
@@ -4,7 +4,14 @@ const pad10 = num => num < 10 ? `0${num}` : num.toString();
 
 // controls take the format YYYY-MM-ddTHH:mm
 const fromUtc = dateStr => {
-  const dt = new Date(Date.parse(dateStr));
+  const dayUtc = new Date(Date.parse(dateStr));
+  const dt = new Date(Date.UTC(
+    dayUtc.getFullYear(),
+    dayUtc.getMonth(),
+    dayUtc.getDate(),
+    dayUtc.getHours(),
+    dayUtc.getMinutes()
+  ));
   return (
     `${dt.getFullYear()}-${pad10(dt.getMonth() + 1)}-${pad10(dt.getDate())}T` +
     `${pad10(dt.getHours())}:${pad10(dt.getMinutes())}`
