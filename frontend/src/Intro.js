9372b51f0a2989c44bea4ffd773647fc8c52a1e8
Jeff Clement <jeffrey.clement@gmail.com>
2021-11-26 12:14:31 -0700

    further wording changes

--- frontend/src/Intro.js
+++ frontend/src/Intro.js
@@ -14,7 +14,7 @@ export default class Intro extends React.Component {
     <div className="upper">
       <p>HELLO RECRUIT!</p>
       <p>You are part of a new digital transformation initiative at the North Pole to roll out a high-tech fleet of robots to help clear the backlog created by disruptions to the global supply chain. To support the holiday effort, you will be assigned a series of computational tasks.  Unfortunately, as everyone knows, elves hate robots and they'll do their best to sabotage your efforts.</p>
-      <p>Are you up to the task? Joints oiled, batteries charged?</p>
+      <p>Are you up to the task?</p>
       <p>Problems above in green are active and ready to be solved. Let's save Christmas!</p>
       <p>01000111 01101111 01101111 01100100 00100000 01001100 01110101 01100011 01101011 00100001</p>
     </div>
