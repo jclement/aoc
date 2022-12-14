6e96045a56cbd4525be89d5d3fab3274e5efbab9
Ryan <ryan.mohrman@gmail.com>
2022-12-13 17:23:06 -0700

    email 'rhyn0bytes@protonmail.com' is also verified

--- frontend/src/VerifiedCheck.js
+++ frontend/src/VerifiedCheck.js
@@ -8,7 +8,7 @@ export default class VerifiedCheck extends React.Component {
       title="Has at least $8/month to spare" />
   </span>);
 
-  isVerified = () => (this.props.user.is_admin || this.props.user.email === 'eli@lilly.fake');
+  isVerified = () => (this.props.user.is_admin || ['eli@lilly.fake', 'rhyn0bytes@protonmail.com'].includes(this.props.user.email));
 
   render = () => this.isVerified() ? this.preRender : null;
 }
