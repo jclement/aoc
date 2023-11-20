390db15c92bde661f30e055903e2a33a29db09fa
Jeff Clement <jeffrey.clement@gmail.com>
2023-11-20 16:49:32 -0700

    updated into for 2023

--- frontend/src/Intro.js
+++ frontend/src/Intro.js
@@ -3,14 +3,9 @@ import { Header } from './Styling';
 
 export default class Intro extends React.Component {
   render = () => (<div className="font-monospace">
-    <h3>Welcome to the Advent of Qode 2022</h3>
-    <h4>Mars Attacks Christmas</h4>
-    <p>The sanctitude of Christmas has yet again been threatened. This year the threat is Mars!</p>
-    <p>For decades now, NASA has been sending probes to mars in search of life and other stuff. Martians apparantly see foreign probes as declarations of war. </p>
-    <p>They have realized the way to conquer the planet is of course to take over Christmas!!!</p>
-    <p>Santa has hired us, a <i>Rag Tag</i> group of robot mercenaries known as the "Bad Baker's Dozen".</p>
-    <p>We are here and ready to kick some Martian butt.</p>
-    <p>Your mission, should you choose to accept it, is to join the ranks of the "Bad Baker's Dozen", help defeat the Martian Invaders and Save Christmas!</p>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/6Uw2UMvOuSQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <h3>Welcome to the Advent of Qode 2023</h3>
+    <h4>The Blob Eats Christmas</h4>
+    <p>We need your help to save Christmas (and the Earth) from Sedrick, a mysterious creature formed from candy canes, maple syrup, and 1.21 gigawatts of electricity.</p>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/A9rpjqb45Q4?si=ViOLoJ2HQPHG9y53" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
   </div>)
 }
